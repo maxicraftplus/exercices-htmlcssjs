eb0371635deb7772cd28b55acdeed6c0c4c0f016
maxicraft plus <maxitrompette@ymail.com>
2018-10-17 17:59:31 +0200

    menu qui s'affiche quand on descend

---
+++ app.js
@@ -0,0 +1,12 @@
+this.onscroll = function() {
+     scrolling();
+};
+
+function scrolling() {
+     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("navbar").style.top = "0";
+     }
+     else {
+          document.getElementById("navbar").style.top = "-50px";
+     }
+}
