dc1697b1e37f476ee68147ac90e97db6816f7933
maxicraft plus <maxitrompette@ymail.com>
2018-10-26 15:28:18 +0200

    gestion du click via addEventListener

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const p1 = document.getElementById("p1");
+const b1 = document.getElementById("b1");
 const tabMonth = ["janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre"];
 
-function showDate() {
+b1.addEventListener("click", function() {
     const date = new Date();
     const currentDate = date.getDate() + " " + tabMonth[date.getMonth()] + " " + date.getFullYear();
     p1.innerHTML += currentDate + "<br>";
-}
+});
