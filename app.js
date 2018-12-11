0884c60c14cd66eb584266084ab38b086ec621fc
maxicraft plus <maxitrompette@ymail.com>
2018-12-11 17:19:40 +0100

    plein ecran

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 function openNav() {
-     document.getElementById("my-sidenav").style.width = "250px";
-     document.getElementById("main").style.marginLeft = "250px";
+     document.getElementById("my-sidenav").style.width = "100%";
      document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
 }
 
 function closeNav() {
      document.getElementById("my-sidenav").style.width = "0";
-     document.getElementById("main").style.marginLeft = "0";
      document.body.style.backgroundColor = "white";
 }
