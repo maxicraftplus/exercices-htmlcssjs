68bd9f714d85252209d1d5f817fe5b97670f6592
maxicraft plus <maxitrompette@ymail.com>
2018-12-11 17:28:22 +0100

    depuis le haut

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function openNav() {
-     document.getElementById("my-sidenav").style.width = "100%";
+     document.getElementById("my-sidenav").style.height = "100%";
      document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
 }
 
 function closeNav() {
-     document.getElementById("my-sidenav").style.width = "0";
+     document.getElementById("my-sidenav").style.height = "0";
      document.body.style.backgroundColor = "white";
 }
