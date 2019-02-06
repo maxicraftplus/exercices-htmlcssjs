22e1b2c5e764a0ecd23cdad94dc35cc3374a389a
maxicraft plus <maxitrompette@ymail.com>
2019-02-06 17:25:40 +0100

    naviguation image modale

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-const modal = document.getElementById("modal");
-const img = document.getElementById("img1");
-const imgModal = document.querySelector(".modal-content");
-const titleModal = document.querySelector("#title");
-const btCloseModal = document.querySelector(".close");
-
-img.addEventListener("click", openModal);
-btCloseModal.addEventListener("click", closeModal);
-
 function openModal() {
-    modal.style.display = "block";
-    imgModal.src = img.src;
-    titleModal.innerHTML = img.alt;
+    document.querySelector(".modal").style = "display: block";
 }
 
 function closeModal() {
-    modal.style.display = "none";
+    document.querySelector(".modal").style = "display: none";
 }
+
+function showSlides(n) {
+    let i;
+    const slides = document.querySelectorAll(".slides");
+    const demos = document.querySelectorAll(".demo");
+    const caption = document.querySelector(".caption");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < demos.length; i++) {
+        demos[i].className = demos[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    demos[slideIndex-1].className += " active";
+    caption.innerHTML = demos[slideIndex-1].alt;
+}
+
+function plusSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlides(n) {
+    showSlides(slideIndex = n);
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
